4c19a764bf2985516ec66130c4268afe7d14c087
JeffreyEarly <jeffrey@jeffreyearly.com>
2022-02-15 07:54:43 -0800

    Create TutorialScript.m

---
+++ TutorialScript.m
@@ -0,0 +1,5 @@
+% Just playing with the data to try to figure out what is going on
+geofile = '/Volumes/Samsung_T5/FromThilo/NISKINEthilo_4km_negLons.nc';
+datafile = '/Volumes/Samsung_T5/FromThilo/niskine_his_00040.nc';
+
+s_rho = ncread(datafile,'s_rho');
